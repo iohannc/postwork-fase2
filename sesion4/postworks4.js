2cadd9d3e7c165cfecb0aa3674a19b792025cd06
Juan Negrete <juan.necastaneda@gmail.com>
2021-07-28 23:40:26 -0500

    Comente los console.logs

--- sesion4/postworks4.js
+++ sesion4/postworks4.js
@@ -63,9 +63,9 @@ function chunk(array, size) {
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log('Test 1:', chunk(data, 1)); // [[1], [2], [3], [4], [5], [6], [7], [8]]
-console.log('Test 2:', chunk(data, 2)); // [[1, 2], [3, 4], [5, 6], [7, 8]]
-console.log('Test 3:', chunk(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
+// console.log('Test 1:', chunk(data, 1)); // [[1], [2], [3], [4], [5], [6], [7], [8]]
+// console.log('Test 2:', chunk(data, 2)); // [[1, 2], [3, 4], [5, 6], [7, 8]]
+// console.log('Test 3:', chunk(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
 
 
 // 3. Función frequency()
