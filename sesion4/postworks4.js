f83640e666cdaab1100f451ba1f9c30c0dfa0873
Juan Negrete <juan.necastaneda@gmail.com>
2021-07-29 21:16:03 -0500

    Descomente los console.logs y agregue el script al html

--- sesion4/postworks4.js
+++ sesion4/postworks4.js
@@ -29,11 +29,11 @@ const john = {
   lastName: "Doe",
 };
 
-// console.log("Test 1:", deepEqual(1, 1)); // true
-// console.log("Test 2:", deepEqual(1, "1")); // false
-// console.log("Test 3:", deepEqual(john, john)); // true
-// console.log("Test 4:", deepEqual(john, { firstName: "John", lastName: "Doe" })); // true
-// console.log("Test 5:", deepEqual(john, { firstName: "John" })); // false
+console.log("Test 1:", deepEqual(1, 1)); // true
+console.log("Test 2:", deepEqual(1, "1")); // false
+console.log("Test 3:", deepEqual(john, john)); // true
+console.log("Test 4:", deepEqual(john, { firstName: "John", lastName: "Doe" })); // true
+console.log("Test 5:", deepEqual(john, { firstName: "John" })); // false
 
 // 2. Función chunk()
 function chunk(array, size) {
@@ -63,9 +63,9 @@ function chunk(array, size) {
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// console.log('Test 1:', chunk(data, 1)); // [[1], [2], [3], [4], [5], [6], [7], [8]]
-// console.log('Test 2:', chunk(data, 2)); // [[1, 2], [3, 4], [5, 6], [7, 8]]
-// console.log('Test 3:', chunk(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
+console.log('Test 1:', chunk(data, 1)); // [[1], [2], [3], [4], [5], [6], [7], [8]]
+console.log('Test 2:', chunk(data, 2)); // [[1, 2], [3, 4], [5, 6], [7, 8]]
+console.log('Test 3:', chunk(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
 
 
 // 3. Función frequency()
@@ -91,9 +91,6 @@ function frequency(string) {
   return dic1;
 }
 
-// console.log("Test 1:", frequency("cccbbbaaa"));
-// {a: 3, b: 3, c: 3}
-// console.log("Test 2:", frequency("www.bedu.org"));
-// {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
-// console.log("Test 3:", frequency("john.doe@domain.com"));
-// {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
+console.log("Test 1:", frequency("cccbbbaaa")); // {a: 3, b: 3, c: 3}
+console.log("Test 2:", frequency("www.bedu.org")); // {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
+console.log("Test 3:", frequency("john.doe@domain.com")); // {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
