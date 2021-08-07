8c75b010f821c9341affcedf4bde5988f1270b23
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-08-06 21:40:46 -0500

    mas formatos de imagen para webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   plugins: [
-      new HtmlWebpackPlugin({
-          filename:'index.html',
-          template: './src/index.html'
-      })
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
   ],
   devServer: {
-      contentBase: path.resolve(__dirname, 'dist')
+    contentBase: path.resolve(__dirname, "dist"),
   },
   module: {
-      rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        },
-        {
-            test: /\.(jpg|png)$/,
-            type: 'asset/resource'
-        },
-        {
-            test: /\.html$/i,
-            loader: "html-loader"
-        }
-      ]
-  }
-}
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
