53bbdf0e73d40f6585d36182a0f111acb70f5513
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-08-06 21:50:28 -0500

    fuentes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         type: "asset/resource",
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.html$/i,
         loader: "html-loader",
       },
