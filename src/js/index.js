49d323c8879b5b6c7456ffeb183380ebc118eaa6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-14 13:56:48 -0500

    pruebas con los eventos del input y boton

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
 import "../css/styles.css";
 import "../index.html";
 
+// Seleción del boton de random
+const btnRandom = document.getElementById('brand');
+
+//Selección del input donde buscará el usuario
+const inputReceta = document.getElementById('site-search');
+
+// Otorgarle un evento al boton random
+btnRandom.addEventListener('click', recetaRandom);
+
+// Otorgarle un evento al input
+inputReceta.addEventListener('keydown', buscarReceta);
+
+
+// Fnción para buscar receta random
+function recetaRandom(event){
+    alert("haz hecho click");
+}
+
+// Funcion para buscar receta
+function buscarReceta(event){
+    if(event.keyCode == '13'){
+        alert("Presionaste Enter al Input");
+    }
+}
