dbdcf877e48e313605932bbad927b96a96940461
Alex Aguilar <zhufany@gmail.com>
2021-08-14 23:11:14 -0500

    Añadí función provisional api

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,23 @@ import "../css/styles.css";
 //import "../output.css"
 import "../index.html";
 
-
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
-.then(response => {
-	return response.json();
+const search = document.querySelector('#site-search');
+search.addEventListener('keyup', function (item) {
+    if (item.key != 'Enter') {
+        console.log(search.value);
+    } else {
+        let busqueda = search.value;
+        console.log(busqueda);
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busqueda}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => console.log(data))
+            .catch(err => {
+                console.error(err);
+            });
+    }
 })
-.then(data => console.log(data))
-.catch(err => {
-	console.error(err);
-});
+
+
+
